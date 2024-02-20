9f60ba3a9b71250d80eda0108e6febd9d9a7690e
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2024-02-20 17:48:41 +0500

    chore: mark version 1.0.2
    added nhentai.net

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,13 +1,15 @@
-export const sites = ['nhentai.to', 'nhentai.xxx', 'nhentai.website'] as const
+export const sites = ['nhentai.to', 'nhentai.net', 'nhentai.xxx', 'nhentai.website'] as const
 
 export const baseURLS = {
     to: 'https://nhentai.to',
+    net: 'https://nhentai.net',
     xxx: 'https://nhentai.xxx',
     website: 'https://nhentai.website'
 }
 
 export const imageSites = {
     to: 'cdn.dogehls.xyz',
+    net: /t[357].nhentai.net/,
     xxx: 'cdn.nhentai.xxx',
     website: 'cdn.dogehls.xyz'
 }
